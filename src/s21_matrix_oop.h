0c0583784f19717103a7b18e747322d5d0bd919c
casimira <casimira@student.21-school.ru>
2023-03-18 19:34:20 +0300

    ALLO YOBA, ETO TI?

--- src/s21_matrix_oop.h
+++ src/s21_matrix_oop.h
@@ -1,30 +1,40 @@
 
 
-#include <iostream>
 #include <cmath>
-#include <cstring>
 #include <cstdio>
+#include <cstring>
+#include <iostream>
 
-class S21Matrix{
-//конструкторы и деструктор
-S21Matrix();
-S21Matrix(const int rows, const int cols);
-S21Matrix(const S21Matrix& other);
-S21Matrix(S21Matrix&& other);
-~S21Matrix();
-bool EqMatrix(const S21Matrix& other);
-void SumMatrix(const S21Matrix& other);
-void SubMatrix(const S21Matrix& other);
-void MulNumber(const double num);
-void MulMatrix(const S21Matrix& other);
-//операторы перегрузки 
-S21Matrix& S21Matrix ::operator=(const S21Matrix&other);
-S21Matrix& S21Matrix ::operator=(S21Matrix&other) noexcept;
-private:
+class S21Matrix {
+  //конструкторы и деструктор
+  S21Matrix();
+  S21Matrix(const int rows, const int cols);
+  S21Matrix(const S21Matrix& other);
+  S21Matrix(S21Matrix&& other);
+  ~S21Matrix();
+  int Get_Rows() const;
+  int Get_Cols() const;
+  void Set_Rows(const int _rows_);
+  void Set_Cols(const int _cols_);
+  void Print_Matrix();
+  void Init_Matrix();
+  bool EqMatrix(const S21Matrix& other);
+  void SumMatrix(const S21Matrix& other);
+  void SubMatrix(const S21Matrix& other);
+  void MulNumber(const double num);
+  void MulMatrix(const S21Matrix& other);
+  S21Matrix Transpose() const;
+  S21Matrix Minor_Create(int _rows_, int _cols_) const;
+  double Determinant() const;
+  S21Matrix CalcComplements() const;
+  //операторы перегрузки
+  S21Matrix& S21Matrix ::operator=(const S21Matrix& other);
+  S21Matrix& S21Matrix ::operator=(S21Matrix& other) noexcept;
+
+ private:
   int rows = 0, cols = 0;
   double** matrix = nullptr;
 
   void MemoryAllocate();
   void MemoryFree();
-
 };
